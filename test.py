af6709edb58d070282c06e602884ce39f1576fc3
Your Name <you@example.com>
2026-03-06 09:49:50 -0500

    Process

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import akshare as ak
 
-option_finance_board_df = ak.option_finance_board(symbol="沪深300股指期权",end_month="202604")
+option_finance_board_df = ak.option_finance_board(symbol="上证50ETF期权",end_month="202604")
 print(option_finance_board_df)
