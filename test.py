3a11e45d77f6cf232a7e48cfea441946d54873ab
Your Name <you@example.com>
2026-03-16 10:48:34 -0400

    Process

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
 import akshare as ak
 
-target_date = "20260213"
-futures_comm_js_df = ak.futures_comm_info(symbol="中国金融期货交易所")
-print(futures_comm_js_df)
-
-if not futures_comm_js_df.empty:
-    filename = f"futures_comm_{target_date}.csv"
-    futures_comm_js_df.to_csv(filename, index=False, encoding='utf-8-sig')
-    print(f"Data saved to {filename}")
-else:
-    print("No data found for the given date.")
+futures_hist_em_df = ak.futures_hist_em(symbol="中证1000股指2603", period="daily")
+print(futures_hist_em_df)
